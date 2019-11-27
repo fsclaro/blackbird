93d656a560fe0037cadc94412800736bb4794b0f
Fernando S Claro <fsclaro@gmail.com>
2019-11-27 19:09:22 -0300

    adição de campos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->boolean('active')->default(1);
+            $table->string('skin')->nullable();
             $table->rememberToken();
 
             $table->timestamps();

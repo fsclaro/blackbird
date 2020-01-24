be03d4971e861f120d3dbd2bd87f8f41719b373e
Fernando Salles Claro <fsclaro@gmail.com>
2020-01-23 21:59:43 -0300

    filter do menu do adminlte

--- app/MenuFilter/SuperAdminFilter.php
+++ app/MenuFilter/SuperAdminFilter.php
@@ -4,7 +4,6 @@ namespace App\Menufilter;
 
 use JeroenNoten\LaravelAdminLte\Menu\Builder;
 use JeroenNoten\LaravelAdminLte\Menu\Filters\FilterInterface;
-use App\User;
 use Auth;
 
 class SuperAdminFilter implements FilterInterface
@@ -12,8 +11,12 @@ class SuperAdminFilter implements FilterInterface
     public function transform($item, Builder $builder)
     {
 
-        if (isset($item['permission']) || Auth::user()->is_superadmin) {
+        if (!isset($item['can']) || Auth::user()->is_superadmin) {
             return $item;
+        } else {
+            if (\Gate::allows($item['can'])) {
+                return $item;
+            }
         }
 
         return false;

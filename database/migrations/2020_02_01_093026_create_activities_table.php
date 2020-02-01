5dfd073875903e57829af8caec20e768b940643b
Fernando Salles Claro <fsclaro@gmail.com>
2020-02-01 10:55:03 -0300

    criaçao de nova migration

---
+++ database/migrations/2020_02_01_093026_create_activities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ipaddress')->nullable();
+            $table->string('externalip')->nullable();
+            $table->string('useragent')->nullable();
+            $table->string('url')->nullable();
+            $table->text('title')->nullable();
+            $table->longText('details')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
